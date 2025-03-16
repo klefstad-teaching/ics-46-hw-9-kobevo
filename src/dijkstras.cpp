7a7f5a7830423180178d48943f7384077027c76f
kobevo <kobevo05@gmail.com>
2025-03-16 08:02:01 +0000

    changed print

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,7 +42,8 @@ vector<int> extract_shortest_path(const vector<int>&, const vector<int>& previou
 }
 
 void print_path(const vector<int>& v, int total) {
-    for (int i = 0; i < total; ++i)
+    int size = v.size();
+    for (int i = 0; i < size; ++i)
         std::cout << v[i] << " ";
     std::cout << std::endl;
     std::cout << "Total cost is " << total << std::endl;
